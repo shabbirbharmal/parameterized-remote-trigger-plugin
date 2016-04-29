974633642c5fe7c5f701cdc8ebac34b74af624ab
danielr928 <daniel_richardson@intuit.com>
2016-04-28 23:29:43 -0700

    enable when crumbs are required

--- src/test/java/org/jenkinsci/plugins/ParameterizedRemoteTrigger/RemoteBuildConfigurationTest.java
+++ src/test/java/org/jenkinsci/plugins/ParameterizedRemoteTrigger/RemoteBuildConfigurationTest.java
@@ -1,6 +1,7 @@
 package org.jenkinsci.plugins.ParameterizedRemoteTrigger;
 
 import hudson.model.FreeStyleProject;
+import hudson.security.csrf.DefaultCrumbIssuer;
 import net.sf.json.JSONObject;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,7 +22,34 @@ public class RemoteBuildConfigurationTest {
 
         String remoteUrl = jenkinsRule.getURL().toString();
         RemoteJenkinsServer remoteJenkinsServer =
-                new RemoteJenkinsServer(remoteUrl, "JENKINS", false, auth);
+                new RemoteJenkinsServer(remoteUrl, "JENKINS", false, false, auth);
+        RemoteBuildConfiguration.DescriptorImpl descriptor =
+                jenkinsRule.jenkins.getDescriptorByType(RemoteBuildConfiguration.DescriptorImpl.class);
+        descriptor.setRemoteSites(remoteJenkinsServer);
+
+        FreeStyleProject remoteProject = jenkinsRule.createFreeStyleProject();
+
+        FreeStyleProject project = jenkinsRule.createFreeStyleProject();
+        RemoteBuildConfiguration remoteBuildConfiguration = new RemoteBuildConfiguration(
+                remoteJenkinsServer.getDisplayName(), false, remoteProject.getFullName(), "",
+                "", true, null, null, false, true, 1);
+        project.getBuildersList().add(remoteBuildConfiguration);
+
+        jenkinsRule.buildAndAssertSuccess(project);
+    }
+
+    @Test
+    public void testRemoteBuildWithCrumb() throws Exception {
+        jenkinsRule.jenkins.setCrumbIssuer(new DefaultCrumbIssuer(true));
+
+        JSONObject authenticationMode = new JSONObject();
+        authenticationMode.put("value", "none");
+        JSONObject auth = new JSONObject();
+        auth.put("authenticationMode", authenticationMode);
+
+        String remoteUrl = jenkinsRule.getURL().toString();
+        RemoteJenkinsServer remoteJenkinsServer =
+                new RemoteJenkinsServer(remoteUrl, "JENKINS", false, true, auth);
         RemoteBuildConfiguration.DescriptorImpl descriptor =
                 jenkinsRule.jenkins.getDescriptorByType(RemoteBuildConfiguration.DescriptorImpl.class);
         descriptor.setRemoteSites(remoteJenkinsServer);
